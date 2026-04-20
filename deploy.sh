a8e875c7ebfd0986f49069658e95b61dd6bbf954
andrei steven deang dela cruz <andreisteven@betonchu.local>
2026-04-20 18:44:44 +0800

    add oldweb button

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,15 @@
 find . -name ".DS_Store" -delete
 
 git add .
-git commit -m "sync"
+
+echo "--- Describe this manifestation (Commit Message) ---"
+read -p "> " commit_msg
+
+if [ -z "$commit_msg" ]; then
+    commit_msg="automated sync"
+fi
+
+git commit -m "$commit_msg"
 git push origin main
 
 neocities push . 
